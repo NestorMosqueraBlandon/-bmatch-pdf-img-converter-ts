9909869962aaf7e969cf28af98428508682f2b0c
Nestor Mosquera <yunsde182gmail.com>
2023-03-30 15:52:52 -0500

    fix: path export type for use

---
+++ node-test-converter/src/index.ts
@@ -0,0 +1,9 @@
+import converter from '@estcode/pdf-img-converter-ts'
+
+
+const main  = async() => {
+ await converter();
+ 
+}
+
+
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,1 @@
-import PDF2img from './pdf2img';
-
-const main = async(pathPdf?: any, width?: number, height?:number) => { 
- const images =  await PDF2img(pathPdf, {
-  width,
-  height
- });
- 
- return images;
-}
-
-main();
+export { default as pdf2img } from './pdf2img';
